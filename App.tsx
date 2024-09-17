8dbe09fa74c6e7a601e99d23205fba37592215c8
dition0221 <129196812+dition0221@users.noreply.github.com>
2024-09-18 00:16:15 +0900

    24-09-17 / Weather App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,112 @@
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { API_KEY } from "@env";
+import axios from "axios";
+import Fontisto from "@expo/vector-icons/Fontisto";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("screen");
+
+const icons: any = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Rain: "rains",
+  Snow: "snow",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+  Atmosphere: "cloudy-gusts",
+};
 
 export default function App() {
+  const [city, setCity] = useState("Loading..");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  useEffect(() => {
+    (async function getWeather() {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        return setOk(false);
+      }
+
+      // city
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      const location = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+      setCity(location[0].region ?? "");
+
+      // fetch
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+      );
+      setDays(data.list);
+    })();
+  }, []);
+
+  const timeFormatter = (dateString: string) => {
+    const date = new Date(dateString);
+    date.setHours(date.getHours() + 9);
+    return date.toISOString().replace("T", " / ").slice(2, 15) + " h";
+  };
+
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
 
       <View style={styles.container}>
-        <Text style={styles.text}>Hello!</Text>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>{city}</Text>
+        </View>
+
+        <ScrollView
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.weather}
+        >
+          {days.length === 0 ? (
+            <View style={{ ...styles.day, alignItems: "center" }}>
+              <ActivityIndicator
+                color="white"
+                size="large"
+                style={styles.loading}
+              />
+            </View>
+          ) : (
+            days.map((day: any, idx) => (
+              <View key={idx} style={styles.day}>
+                <Text style={styles.time}>{timeFormatter(day.dt_txt)}</Text>
+                <View style={styles.tempContainer}>
+                  <Text style={styles.temp}>
+                    {parseFloat(day.main.temp).toFixed(1)}
+                  </Text>
+                  <Fontisto
+                    name={icons[day.weather[0].main]}
+                    size={68}
+                    color="white"
+                  />
+                </View>
+
+                <Text style={styles.main}>{day.weather[0].main}</Text>
+                <Text style={styles.description}>
+                  {day.weather[0].description}
+                </Text>
+              </View>
+            ))
+          )}
+        </ScrollView>
       </View>
     </>
   );
@@ -16,13 +115,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "royalblue",
   },
-  text: {
-    fontSize: 28,
-    color: "blue",
-    fontWeight: "900",
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "white",
+    fontSize: 58,
+    fontWeight: "500",
+  },
+  weather: {
+    flexGrow: 1,
+    backgroundColor: "red",
+  },
+  loading: {
+    marginTop: 10,
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    paddingHorizontal: 20,
+  },
+  time: {
+    color: "white",
+    fontSize: 18,
+    marginBottom: -20,
+  },
+  tempContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  temp: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 88,
+  },
+  main: {
+    color: "white",
+    fontSize: 24,
+  },
+  description: {
+    color: "white",
+    fontSize: 18,
   },
 });
--- README.md
+++ README.md
@@ -12,3 +12,69 @@
 ---
 
 - **24-09-16 : #1.1 ~ #2.3 / Introduction + 3rd-party components & API**
+  - 설치 S/W
+    - React-Native는 휴대폰 App을 만드는 것이라서 추가적인 S/W가 필요함
+    - Android
+      - Android Studio
+      - JAVA
+      - Android SDK
+      - Simulator
+    - iOS (MacOS에서 사용해야 함)
+      - XCode
+      - Simulator
+  - React-Native
+    - 코드와 OS 사이에 있는 interface
+      - 코드는 iOS 또는 JAVA Android 코드로 번역이 됨
+    - React-Native가 실제로 element를 렌더링하는 것이 아니고, OS에게 렌더링 해달라고 요청하며 OS가 렌더링을 함
+      - 휴대폰 안에 존재하는 브라우저가 아님
+  - Expo
+    - 추가적인 S/W를 사용할 필요없이 VSCode를 쓰고, 휴대폰에서 App을 바로 test할 수 있음
+      - 테스트 목적(프로토타입)으로 많이 사용함
+      - 직접 출시할 예정이라면, 추가적인 S/W를 설치해야 함
+    - 설치법
+      1. `npm i -g expo-cli`로 cli 환경 설치하기
+         - Mac 사용자라면, `Watchman`을 추가적으로 설치
+           ```
+           brew update
+           brew install watchman
+           ```
+      2. apple store(`Expo Go`) 또는 google play store(`Expo`)에서 다운받기
+    - 프로젝트 설치법
+      1. `npx create-expo-app@latest 프로젝트명 --template`으로 프로젝트 설치하기
+         - `blank` 선택하기
+      2. `expo login`으로 cli에서 로그인하기
+      3. `npm start`로 테스트 실행하기
+    - <a href="https://expo.dev" target="_blank">홈페이지</a>
+  - Snack
+    - 브라우저에서 React-Native App을 만들 수 있게 해주는 온라인 코드 에디터
+    - <a href="https://snack.expo.dev" target="_blank">홈페이지</a>
+  - `<View />`
+    - `<div />` 대신 사용하는 element
+  - `<Text />`
+    - text 사용 시 필수로 사용하는 element
+  - style
+    - 일부 style을 사용할 수 없음 (border 등)
+    - 사용법 : `const styles = StyleSheet.create(스타일객체);`
+      - ex.
+        ```
+        <View styles={styles.container} />
+        const styles = StyleSheet.create({
+          container: {
+            flex: 1,
+            backgroundColor: "#fff",
+          },
+        });
+        ```
+    - 자동 완성 기능 제공
+      - 그냥 객체로 사용해도 되나, 자동 완성 기능을 제공하지 않음
+  - StatusBar
+    - 3rd-party 패키지로부터 사용
+  - 3rd-party Components & API
+    - React-Native는 필수로 필요로하는 component와 API만 제공
+      - 유지 관리와 업데이트가 힘들기 때문
+    - 다른 기능들은 커뮤니티 패키지를 사용해야 함
+    - <a href="https://reactnative.directory" target="_blank">홈페이지</a>
+  - Expo SDK
+    - Expo 팀이 자체적으로 Packages와 APIs를 만들어 배포함
+    - <a href="https://docs.expo.dev/versions/latest"           target="_blank">홈페이지</a>
+- **24-09-17 : #2.4 ~ #2.10 / Weather App**
